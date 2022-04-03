0c99f26b48acbc64747d3a0e9e2cda2490d88a6f
Suxhita <81512383+Suxhita@users.noreply.github.com>
2022-04-03 17:53:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+status = "";
+img = "";
+
+
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = 'Status : Detecting Objects';
 }
 
 function preload(){
@@ -14,4 +20,24 @@ function draw(){
     noFill();
     stroke('#00ffff');
     rect(30,60,450,350);
+
+    fill('#00ffff')
+    text('Cat', 320, 120);
+    noFill();
+    stroke('#00FFFF');
+    rect(300, 90, 270, 320);
 }
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+
